99c80da2ad1b3b4733df4b53b8c32b49685c67c8
Juan Petter (Petter) <petter2001us@gmail.com>
2025-10-17 16:37:48 +0000

    App nickname mainserver App ID 1:858364841517:web:1e7a6a3e5c801a31e832a

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -6,11 +6,11 @@ import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMG7_GWoVNP7wuTzYIJYZMffwN7Nej7jg",
-  authDomain: "studio-4707171117-1526d.firebaseapp.com",
-  projectId: "studio-4707171117-1526d",
-  storageBucket: "studio-4707171117-1526d.appspot.com",
-  messagingSenderId: "834835769907",
-  appId: "1:834835769907:web:b4aec87f2f482cb0295146"
+  authDomain: "studio-4707171117-fa17b.firebaseapp.com",
+  projectId: "studio-4707171117-fa17b",
+  storageBucket: "studio-4707171117-fa17b.appspot.com",
+  messagingSenderId: "858364841517",
+  appId: "1:858364841517:web:1e7a6a3e5c801a31e832a4"
 };
 
 // Singleton pattern to ensure Firebase is initialized only once
